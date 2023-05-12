05dae2cb2c76dbad7b1ae30333df7afde1757f0e
LHMAIME <mohamed.lhmaime@sogeti.com>
2023-05-12 18:49:01 +0200

    second commit

--- src/main/java/projet1/patientsmvc/entities/Patient.java
+++ src/main/java/projet1/patientsmvc/entities/Patient.java
@@ -1,10 +1,15 @@
 package projet1.patientsmvc.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 @Entity
@@ -12,10 +17,14 @@ import java.util.Date;
 public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty
+    @Size(min=4, max=40)
     private String nom;
     @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern="yyyy-mm-dd")
     private Date dateNaissance;
     private boolean malade;
+    @DecimalMin("100")
     private int score;
 
 }
--- src/main/java/projet1/patientsmvc/web/PatientController.java
+++ src/main/java/projet1/patientsmvc/web/PatientController.java
@@ -1,12 +1,15 @@
 package projet1.patientsmvc.web;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import projet1.patientsmvc.entities.Patient;
@@ -17,29 +20,30 @@ import java.util.List;
 @Controller @AllArgsConstructor
 public class PatientController {
     private PatientRepository patientRepository;
-    @GetMapping(path="/index")
+
+    @GetMapping(path = "/index")
     public String patients(Model model,
-                           @RequestParam(name= "page", defaultValue = "0") int page,
+                           @RequestParam(name = "page", defaultValue = "0") int page,
                            @RequestParam(name = "size", defaultValue = "5") int size,
                            @RequestParam(name = "keyword", defaultValue = "") String keyword
-    ){
-        Page<Patient> pagePatients = patientRepository.findByNomContains(keyword,PageRequest.of(page,size));
-        model.addAttribute("listPatients",pagePatients.getContent());
-        model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
-        model.addAttribute("currentPage",page);
-        model.addAttribute("keyword",keyword);
+    ) {
+        Page<Patient> pagePatients = patientRepository.findByNomContains(keyword, PageRequest.of(page, size));
+        model.addAttribute("listPatients", pagePatients.getContent());
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("keyword", keyword);
         return "patients";
     }
 
     //GetMapping a eviter en suppression de donnees (securite): utiliser la methode delete ou post
     @GetMapping("/delete")
-    public String delete(Long id, String keyword, int page){
+    public String delete(Long id, String keyword, int page) {
         patientRepository.deleteById(id);
-        return "redirect:/index?page="+page+"&keyword="+keyword;
+        return "redirect:/index?page=" + page + "&keyword=" + keyword;
     }
 
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "redirect:/index";
     }
 
@@ -47,7 +51,34 @@ public class PatientController {
     //je crée une methode qui retourne la liste des patients en format Json
     @GetMapping("/patients")
     @ResponseBody
-    public List<Patient> listPatients(){
+    public List<Patient> listPatients() {
         return patientRepository.findAll();
     }
+
+    @GetMapping("/formPatients")
+    public String formPatients(Model model) {
+        model.addAttribute("patient", new Patient());
+        return "formPatients";
+    }
+
+    @PostMapping("/save")
+    public String save(Model model, @Valid Patient patient, BindingResult bindingResult,
+                       @RequestParam(defaultValue = "0") int page,
+                       @RequestParam (defaultValue = "") String keyword){
+
+        if (bindingResult.hasErrors()) return "formPatients";
+        patientRepository.save(patient);
+        return "redirect:/index?page="+page +"&keyword="+keyword;
+    }
+
+    @GetMapping("/editPatient")
+    public String editPatient(Model model, Long id,String keyword, int page) {
+        Patient patient =patientRepository.findById(id).orElse(null);
+        if(patient==null) throw new RuntimeException("patient introuvable");
+        model.addAttribute("patient", patient);
+        model.addAttribute("page",page);
+        model.addAttribute("keyword",keyword);
+        return "editPatient";
+    }
+
 }
